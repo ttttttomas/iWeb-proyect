c4746a4e519965e90d5005a5dc01eab9791d020a
Tomas <totobarajas124@gmail.com>
2024-07-07 16:41:52 -0300

    add header 1.0

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors:{
+				bgColor1: "rgb(38, 39, 43)",
+				bgColor2: "rgb(31, 32, 35)",
+        		bgColor3: "rgb(48, 49, 54)",
+
+			}
+		},
 	},
 	plugins: [],
 }
